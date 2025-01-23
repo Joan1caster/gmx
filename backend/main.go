0b47ee79303a8847366c01943ab54ba994d9664a
joan <xibirajoan@outlook.com>
2025-01-23 09:59:17 +0800

    risk block

--- backend/main.go
+++ backend/main.go
@@ -16,7 +16,6 @@ func main() {
 
 	go service.GetPrice("btcusdt") // 订阅币安的BTC/USDT价格
 	go service.HandlerOrderInfo()  // 订阅订单信息
-	go service.HandlerPriceInfo()
 
 	// go service.UpdatePriceToChain(priceChain) // 将价格更新到链上
 	defer rabbitmq.Shutdown()
