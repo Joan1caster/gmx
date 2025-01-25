377e74b5cfe3b6bf2a70579c9a6c88ed7caf23b5
azgy <zgy56275@hotmail.com>
2025-01-25 20:29:53 +0800

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -34,7 +34,7 @@ func main() {
 	go service.GetPrice("btcusdt")           // 订阅币安的BTC/USDT价格
 	go orderService.HandlerOrderInfo()       // 订阅订单信息
 	go positionService.HandlerPositionInfo() // 处理资产信息
-	// go service_.HandlerPriceInfo()
+	go service_.HandlerPriceInfo()
 
 	// go service.UpdatePriceToChain(priceChain) // 将价格更新到链上
 	defer rabbitmq.Shutdown()
